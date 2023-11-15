829cbe9d8d1db823a78f7f7512af9df263835a01
Daniel Hjartland <squashical@gmail.com>
2023-11-15 08:39:12 +0100

    remove cobra and add bubbletea
    Decided to go for a TUI solution for interfacing with the server. I may
    decide cobra is appropriate to re-integrate, but just to keep the
    boilerplate down to a minimum and to keep focused on the task as hand,
    I've removed the old boilerplate that was added with cobra-cli.

--- cmd/boilerplater/main.go
+++ cmd/boilerplater/main.go
@@ -1,62 +1,17 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 	"os"
-	"path/filepath"
-	"time"
 
-	"github.com/SQUASHD/boilerplater/internal/shared/httpclient"
-	"github.com/SQUASHD/boilerplater/internal/shared/models"
-	"github.com/SQUASHD/boilerplater/pkg/boilerplate"
-	"github.com/SQUASHD/boilerplater/pkg/markdown"
-	"github.com/SQUASHD/boilerplater/pkg/projectgen"
+	"github.com/SQUASHD/boilerplater/internal/cli/ui"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	type ProjectData struct {
-		ProjectStructure    models.ProjectStructure      `json:"projectStructure"`
-		FunctionBoilerplate []models.FunctionBoilerplate `json:"functionBoilerplate"`
-		AdvancedProject     markdown.ExperiencedProj     `json:"advancedProject"`
+	p := tea.NewProgram(ui.InitialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
-
-	projectDir, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("Error getting current working directory: %v", err)
-	}
-	url := flag.String("url", "", "URL to fetch JSON from")
-	flag.Parse()
-
-	fmt.Println(os.Args)
-	if *url == "" {
-		log.Fatal("No URL provided")
-	}
-
-	var projectData ProjectData
-	httpClient := httpclient.NewHTTPClient(2 * time.Minute)
-	if err = httpClient.Get(nil, *url, &projectData); err != nil {
-		log.Fatalf("Error fetching JSON: %v", err)
-	}
-
-	mdgen := markdown.MarkdownGenerator{}
-
-	if err = projectgen.GenerateProjectStructure(projectData.ProjectStructure); err != nil {
-		log.Fatalf("Error generating project structure: %v", err)
-	}
-	// Generate the project structure
-	boilerplate.GenerateFunctionBoilerplate(projectData.FunctionBoilerplate)
-
-	// Write the md file based on mdgen output
-	mdFilePath := filepath.Join(projectDir, "README.md")
-	_, err = os.Create(mdFilePath)
-	if err != nil {
-		log.Fatalf("Error creating md file: %v", err)
-	}
-	err = os.WriteFile(mdFilePath, []byte(mdgen.GenerateMarkdown(projectData.AdvancedProject)), 0644)
-	if err != nil {
-		log.Fatalf("Error writing md file: %v", err)
-	}
-
 }
